b8c25b1ac70faa25bef99bc3b41204008fbcf603
Erwann Millon <erwannmillon@gmail.com>
2023-07-26 01:17:54 -0400

    v2

---
+++ alerting.py
@@ -0,0 +1,52 @@
+import datetime
+import json
+import os
+import time
+import traceback
+
+import dotenv
+import requests
+from supabase import create_client
+from tqdm import tqdm
+
+dotenv.load_dotenv()
+
+def get_supabase_client():
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+    return supabase
+
+supabase = get_supabase_client()
+def create_supabase_row(data, table_name="cluster_errors"):
+    # try a few times bc the supabase python package is sketchy
+    for i in range(5):
+        try:
+            supabase = get_supabase_client()
+            response = supabase.table(table_name).insert(data).execute()
+            return
+        except:
+            print("SUPABASE ERROR")
+            traceback.print_exc()
+        time.sleep(1)
+
+
+def send_telegram_notification(message):
+    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    print(TELEGRAM_BOT_TOKEN)
+    TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    try:
+        error = {
+            "error_message": message,
+        }
+        create_supabase_row(error)
+        telegram_api_url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+        data = {'chat_id': TELEGRAM_CHAT_ID, 'text': message}
+        response = requests.post(telegram_api_url, json=data)
+        if response.status_code != 200:
+            print('Failed to send Telegram notification:', response.content)
+    except:
+        print("caught exception: Failed to send Telegram notification")
+        traceback.print_exc()
+send_telegram_notification("error 2")
+
--- app.py
+++ app.py
@@ -1,45 +1,45 @@
-import json
+import asyncio
 import os
+from datetime import datetime, timedelta
 
-import dotenv
-import fastapi
-import requests
-from fastapi import FastAPI
-
-app = FastAPI()
-
-dotenv.load_dotenv()
-# from pyngrok import ngrok
-
+from aiogram import Bot
+from supabase_utils import get_supabase_client
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-def send_telegram_notification(message):
-    telegram_api_url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-    data = {'chat_id': TELEGRAM_CHAT_ID, 'text': message}
-    response = requests.post(telegram_api_url, json=data)
-    if response.status_code != 200:
-        print('Failed to send Telegram notification:', response.content)
+print(TELEGRAM_BOT_TOKEN)
+ERROR_THRESHOLD = 20
+TIME_WINDOW_SECONDS = 60
+TIME_WINDOW = timedelta(minutes=TIME_WINDOW_SECONDS*60)
+POLL_INTERVAL = 3  # seconds
 
-@app.route('/', methods=['POST'])
-async def handle_webhook(req: fastapi.Request) -> None:
-    print(TELEGRAM_BOT_TOKEN)
-    print(TELEGRAM_CHAT_ID)
-    # data = json.loads(req.data)
-    # data = await req
-    data = await req.json()
 
-    print(data)
-    alert = data['incident']['condition_name']
-    message = f'Google Cloud Monitoring alert: {alert}'
-    send_telegram_notification(message)
-    return fastapi.Response(status_code=200)
+supabase = get_supabase_client()
+bot_token = TELEGRAM_BOT_TOKEN
+emergency_chat_id = TELEGRAM_CHAT_ID
+bot = Bot(token=bot_token)
+from dateutil.parser import parse
 
-@app.route('/test')
-def test(x):
-    return "healthy"
 
-    
+async def poll_errors():
+    fucked = False
+    while True:
+        response = supabase.table('cluster_errors').select('created_at').order("created_at", desc=True).limit(ERROR_THRESHOLD).execute().data
+        print(len(response))
+        print(response)
+        error_timestamps = sorted(
+            (parse(row['created_at']) for row in response)
+        )[:ERROR_THRESHOLD]
+        oldest, newest = error_timestamps[0], error_timestamps[-1]
+        delta = newest - oldest
+        print(delta)
+        if (newest - oldest) <= TIME_WINDOW:
+            print("error rate exceeded threshold")
+            # await bot.send_message(chat_id=emergency_chat_id, text='Error rate exceeded threshold https://media.giphy.com/media/QmKySYr0lCsrC/giphy.gif')
+            # send_telegram_notification()
+            await asyncio.sleep(TIME_WINDOW_SECONDS)
+        await asyncio.sleep(POLL_INTERVAL)
+
 if __name__ == '__main__':
-    app.run()
+    asyncio.run(poll_errors())
